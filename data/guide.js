9aba506b14f22f2acfbc479b9bf249573386968f
AdrianNoces <142280131+AdrianNoces@users.noreply.github.com>
2024-01-14 11:52:58 +0000

    update

--- data/guide.js
+++ data/guide.js
@@ -1,3 +1,10 @@
 export let GuideContent = `
-    <div><<
+<div class="container">
+<div class="item">
+    <img src="./data/img/artPrinciple/balance.jpg" alt="">
+    <div>
+        <p>Balance</p>
+    </div>
+</div>
+</div>
 `;
